9c2cd815e551e8715a01a270f02216adaae26efe
Reiner Marquez <rmaceissoft@gmail.com>
2019-09-19 16:16:02 -0500

    solve RemovedInDjango18Warning and RemovedInDjango19Warning warnings
    - RemovedInDjango19Warning: django.contrib.contenttypes.generic is deprecated and will be removed in Django 1.9. Its contents have been moved to the fields, forms, and admin submodules of django.contrib.contenttypes.
    - RemovedInDjango18Warning: `VoteManager.get_query_set` method should be renamed `get_queryset`.

--- djangoratings/models.py
+++ djangoratings/models.py
@@ -1,8 +1,8 @@
 from datetime import datetime
 
 from django.db import models
+from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
-from django.contrib.contenttypes import generic
 from django.contrib.auth.models import User
 
 try:
@@ -25,7 +25,7 @@ class Vote(models.Model):
 
     objects         = VoteManager()
 
-    content_object  = generic.GenericForeignKey()
+    content_object  = GenericForeignKey()
 
     class Meta:
         unique_together = (('content_type', 'object_id', 'key', 'user', 'ip_address', 'cookie'))
@@ -56,7 +56,7 @@ class Score(models.Model):
     score           = models.IntegerField()
     votes           = models.PositiveIntegerField()
     
-    content_object  = generic.GenericForeignKey()
+    content_object  = GenericForeignKey()
 
     class Meta:
         unique_together = (('content_type', 'object_id', 'key'),)
@@ -84,7 +84,7 @@ class IgnoredObject(models.Model):
     content_type    = models.ForeignKey(ContentType)
     object_id       = models.PositiveIntegerField()
     
-    content_object  = generic.GenericForeignKey()
+    content_object  = GenericForeignKey()
     
     class Meta:
         unique_together = (('content_type', 'object_id'),)
